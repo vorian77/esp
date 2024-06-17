d4142b75becb27683700df2cd900cb85c5d3dbac
Phyllip Hall <phall776@gmail.com>
2024-06-17 10:16:42 -0400

    fix: deploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,8 @@
 	import '../app.postcss'
 
 	// overlays
-	import OverlayDrawer from '$comps/Overlay/OverlayDrawer.svelte'
-	import OverlayModal from '$comps/Overlay/OverlayModal.svelte'
+	import OverlayDrawer from '$comps/overlay/OverlayDrawer.svelte'
+	import OverlayModal from '$comps/overlay/OverlayModal.svelte'
 	import { Toast, initializeStores } from '@skeletonlabs/skeleton'
 	initializeStores()
 
