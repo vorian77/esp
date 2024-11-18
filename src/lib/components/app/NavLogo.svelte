806cb8ad2e66d4eca7b9a643efd7cc801cdb2073
Phyllip Hall <phall776@gmail.com>
2024-11-18 13:00:48 -0500

    sanity: dbsync problem

--- src/lib/components/app/NavLogo.svelte
+++ src/lib/components/app/NavLogo.svelte
@@ -4,7 +4,7 @@
 
 	export let user: User | undefined
 
-	const FILENAME = '$comps/app/NavLogo.svelte'
+	const FILENAME = '$comps/app/NavLogo.svelte1'
 
 	$: userOrg = user?.org as UserOrg
 	$: classImg = userOrg ? ` mr-${userOrg.logoMarginRight}` : ''
