71541587bf2ea6e9fe465e1ddaa52a71a226724a
Phyllip Hall <phall776@gmail.com>
2024-12-06 05:13:08 -0500

    feat: tailwind UI breadcrumb

--- src/lib/components/app/NavCrumbs.svelte
+++ src/lib/components/app/NavCrumbs.svelte
@@ -22,24 +22,88 @@
 	}
 </script>
 
-<ol class="breadcrumb">
-	{#each crumbsList as item, i}
-		{@const label = item.label}
-		{#if i < crumbsList.length - 1}
-			<li class="crumb">
-				<button
-					class="anchor"
-					role="link"
-					tabindex="0"
-					on:click={() => onClick(i)}
-					on:keyup={() => onClick(i)}
-				>
-					{label}
-				</button>
-			</li>
-			<li class="crumb-separator" aria-hidden>&rsaquo;</li>
-		{:else}
-			<li class="crumb">{label}</li>
-		{/if}
-	{/each}
-</ol>
+<nav class="flex border-b border-gray-200 bg-white" aria-label="Breadcrumb">
+	<ol
+		role="list"
+		class="mx-auto flex w-full max-w-(--breakpoint-xl) space-x-4 px-4 sm:px-6 lg:px-8"
+	>
+		{#each crumbsList as item, i}
+			{@const label = item.label}
+			{#if i === 0}
+				<!-- first -->
+				<li class="flex">
+					<div class="flex items-center">
+						<a
+							href="#"
+							class="text-gray-400 hover:text-gray-500"
+							on:click={() => onClick(i)}
+							on:keyup={() => onClick(i)}
+						>
+							<svg
+								class="size-5 shrink-0"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								aria-hidden="true"
+								data-slot="icon"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+							<span class="sr-only">{label}</span>
+						</a>
+					</div>
+				</li>
+			{:else if i === crumbsList.length - 1}
+				<!-- last -->
+				<li class="flex">
+					<div class="flex items-center">
+						<svg
+							class="h-full w-6 shrink-0 text-gray-200"
+							viewBox="0 0 24 44"
+							preserveAspectRatio="none"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+						</svg>
+						<a
+							href="#"
+							class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+							aria-current="page"
+							on:click={() => onClick(i)}
+							on:keyup={() => onClick(i)}
+						>
+							{label}
+						</a>
+					</div>
+				</li>
+			{:else}
+				<!-- default -->
+				<li class="flex">
+					<div class="flex items-center">
+						<svg
+							class="h-full w-6 shrink-0 text-gray-200"
+							viewBox="0 0 24 44"
+							preserveAspectRatio="none"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+						</svg>
+						<a
+							href="#"
+							class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+							on:click={() => onClick(i)}
+							on:keyup={() => onClick(i)}
+						>
+							{label}
+						</a>
+					</div>
+				</li>
+			{/if}
+		{/each}
+	</ol>
+</nav>
